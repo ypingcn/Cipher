78e7f641d2ff024b8df7fbc1ebb57f38842536de
ypingcn <1344632698@qq.com>
2017-12-17 17:02:52 +0800

    i18n:add zh-cn translation

---
+++ i18n/zh-cn.ts
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.ui" line="23"/>
+        <source>Cipher @ypingcn</source>
+        <translation>加解密程序 @ ypingcn</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="40"/>
+        <source>Copy Plain Text</source>
+        <translation>复制原文</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="53"/>
+        <source>Caesar</source>
+        <translation>凯撒加密</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="60"/>
+        <source>Playfair</source>
+        <translation>Playfair 加密</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="67"/>
+        <source>Hill</source>
+        <translation>Hill 加密</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="74"/>
+        <source>Playfair&apos;s word</source>
+        <translation>Playfair 加密用单词</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="88"/>
+        <source>Hill&apos;s secret key ( 3x3 only)</source>
+        <translation>Hill 加密用矩阵（仅限3*3)）</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="98"/>
+        <source>Load Plain Text From File</source>
+        <translation>从文件加载原文</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="118"/>
+        <source>Encrypt --&gt;&gt;</source>
+        <translation>加密 --&gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="125"/>
+        <source>&lt;&lt;-- Decrypt</source>
+        <translation>&lt;&lt;-- 解密</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="145"/>
+        <source>About</source>
+        <translation>关于本程序</translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="161"/>
+        <source>Copy Cipher Text</source>
+        <translation>复制密文</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="45"/>
+        <source>Choose File</source>
+        <translation>选择文件</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="45"/>
+        <source>All File(*.*)</source>
+        <translation>All File(*.*)</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="51"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="51"/>
+        <source>read file error:%1</source>
+        <translation>文件 %1 读取错误</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="75"/>
+        <source>A Qt-based program for Caesar,Playfair,Hill encryption and decryption.</source>
+        <translation>一个基于 Qt 的凯撒、Playfiar、Hill 加解密程序。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="76"/>
+        <source>&lt;a href=&quot;https://github.com/ypingcn/Cipher&quot;&gt; %1 &lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://github.com/ypingcn/Cipher&quot;&gt; %1 &lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="76"/>
+        <source>click to know more.</source>
+        <translation>点击链接了解更多信息。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="77"/>
+        <source>
+The program is licensed under version 3 of the GNU General Public License.</source>
+        <translation>
+程序基于 GPLv3 开源（代码的开源/免费使用和引用/修改/衍生代码的开源/免费使用，不允许修改后和衍生的代 码做为闭源的商业软件发布和销售，修改和衍生的代码要使用同样的GPL协议开源）。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="78"/>
+        <source>Copyright 2017 ypingcn</source>
+        <translation>版权所有 2017 ypingcn</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="89"/>
+        <source>type error</source>
+        <translation>加（解）密类型未选择</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "widget.h"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator Translator;
+    if(QSysInfo::kernelType() == "linux")
+        Translator.load("/usr/bin/cipher/i18n/zh-cn.qm");
+    else
+        Translator.load("zh-cn.qm");
+	a.installTranslator(&Translator);
+
     Widget w;
     w.show();
 
